9d6529e9d1c55dacea55301a781b9911049ba80d
ornkkk <ornk3.2104@gmail.com>
2020-08-30 14:10:50 +0530

    v1
    Basic Version

--- Test.py
+++ Test.py
@@ -1,32 +1,16 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QPixmap, QFont
 
 
-class EmptyWindow(QWidget):
-    def __init__(self):
-        super().__init__() # create default constructor for QWidget
-        self.initializeUI()
-    
-
-    def initializeUI(self):
-        """
-        Initialize the window and display its contents to the screen.
-        """
-        self.setGeometry(100, 100, 400, 300)
-        self.setWindowTitle('Empty Window in PyQt')
-        self.show()
-
-# Run program
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   window = EmptyWindow()
-   sys.exit(app.exec_())
-   
-   # Above code is same as
-   #app = QApplication(sys.argv)
-   #window = QWidget()
-   #window.setGeometry(100, 100, 400, 300)
-   #window.setWindowTitle('Empty Window in PyQt')
-   #window.show()
-   #sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setGeometry(100, 100, 400, 300)
+    window.setWindowTitle('Test GUI')
+    text = QLabel(window)
+    text.setText("Testing")
+    text.move(105, 15)
+    window.show()
+    sys.exit(app.exec_())
 
